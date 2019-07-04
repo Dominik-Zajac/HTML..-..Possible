bf07e5c091aa17779fd043915179dd61910f01be
Wyrafinowany <zajac_dominik@onet.pl>
2019-07-04 10:41:22 +0200

    Update date in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,9 @@
 /* Time on main page */
 const data = new Date();
 const boxTime = document.getElementById('time');
+const day = data.getDay() < 10 ? 0 : '';
 const months = ["January", "February", "March", "April", "May",
     "June", "July", "August", "September", "October", "November", "December"
 ];
 
-boxTime.innerHTML = `${data.getDay()} ${months[data.getMonth()]} ${data.getFullYear()}`;
+boxTime.innerHTML = `${day}${data.getDay()} ${months[data.getMonth()]} ${data.getFullYear()}`;
